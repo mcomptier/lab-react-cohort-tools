2e6ab9c81b0411b3d1bbe8cf28b0e854e9fd03de
mcomptier <labassecourtmusic@gmail.com>
2023-12-13 16:00:23 +0100

    lab finish

--- src/pages/StudentDetailsPage.jsx
+++ src/pages/StudentDetailsPage.jsx
@@ -1,12 +1,12 @@
 import placeholderImage from "./../assets/placeholder.png";
 import studentsData from "./../assets/students.json";
+import { Link, useParams } from "react-router-dom";
 
 function StudentDetailsPage() {
-
+  const { studentId } = useParams();
   // Find the current student profile by id.
   // In this case, the student with id 1. The `studentId` is hard-coded for now.
   // This could be a URL parameter from React Router, e.g. /students/:studentId
-  const studentId = "1";
   const studentProfile = studentsData.find((student) => student._id === studentId);
 
   return (
@@ -66,10 +66,10 @@ function StudentDetailsPage() {
             </div>
 
 
-            {/* Back button */}
+            <Link to="/"> 
             <button className="text-white px-4 py-2 rounded bg-green-500 hover:bg-green-600 transition duration-300 ease-in-out">
               Back
-            </button>
+            </button></Link> 
 
           </>
         )}
